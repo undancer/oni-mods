856085df0c5f7a3dd8694ab1964ace2cad4d404e
undancer <undancer@gmail.com>
2019-10-03 19:38:41 +0800

    初始化提交

---
+++ refresh/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Harmony;
+
+namespace refresh
+{
+ //   [HarmonyPatch(typeof(ImmigrantScreen),"OnRejectAll")]
+    [HarmonyPatch(typeof(InterfaceTool),"ActivateTool")]
+    public class Class1
+    {
+
+        public static void Prefix()
+        {
+            Debug.Log("SUCCESS !!");
+            Console.WriteLine("SUCCESS !!");
+
+        }
+    }
+}
