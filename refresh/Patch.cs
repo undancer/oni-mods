3e19ce057a9c3012f3b8dfb7c7b15fd8effe01dd
undancer <undancer@gmail.com>
2019-10-04 01:37:13 +0800

    添加默认代码

---
+++ refresh/Patch.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+using System.Linq;
+using Harmony;
+
+namespace refresh
+{
+    public static class ImmigrantScreenContext
+    {
+        public static CarePackageInfo LastSelectedCarePackageInfo { get; set; }
+    }
+
+    [HarmonyPatch(typeof(ImmigrantScreen), "OnProceed")]
+    internal class ImmigrantScreenOnProceed
+    {
+        public static void Prefix(ImmigrantScreen __instance)
+        {
+            var selectedDeliverable = Traverse.Create(__instance).Field("selectedDeliverables")
+                .GetValue<List<ITelepadDeliverable>>().First();
+            if (!(selectedDeliverable is CarePackageInfo carePackageInfo)) return;
+            ImmigrantScreenContext.LastSelectedCarePackageInfo = carePackageInfo;
+        }
+    }
+
+    [HarmonyPatch(typeof(Immigration), "RandomCarePackage")]
+    internal class ImmigrationRandomCarePackage
+    {
+        public static bool Prefix(Immigration __instance, ref CarePackageInfo __result)
+        {
+            var lastSelectedCarePackageInfo = ImmigrantScreenContext.LastSelectedCarePackageInfo;
+            if (lastSelectedCarePackageInfo == null)
+            {
+                return true;
+            }
+
+            __result = lastSelectedCarePackageInfo;
+            ImmigrantScreenContext.LastSelectedCarePackageInfo = null;
+            return false;
+        }
+    }
+}
