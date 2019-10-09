7699efc6dc888e875b14641c7bd9451eea2a7f70
undancer <undancer@gmail.com>
2019-10-10 03:49:45 +0800

    修改按钮文字

--- SelectLastCarePackage/ModUtils.cs
+++ SelectLastCarePackage/ModUtils.cs
@@ -8,6 +8,13 @@ namespace SelectLastCarePackage
         private static readonly List<Label> Mods = Global.Instance.modManager.mods.FindAll(mod => mod.enabled)
             .ConvertAll(mod => mod.label);
 
+        private const string RefreshModId = "1724518038";
+
+        public static bool HasRefreshMod()
+        {
+            return HasMod(RefreshModId);
+        }
+
 
         public static bool HasMod(string id)
         {
--- SelectLastCarePackage/Refresh.cs
+++ SelectLastCarePackage/Refresh.cs
@@ -1,26 +1,34 @@
 using System.Collections.Generic;
 using System.Linq;
 using Harmony;
+using STRINGS;
 using UnityEngine;
 
 namespace SelectLastCarePackage
 {
+    [HarmonyPatch(typeof(ImmigrantScreen), "OnSpawn")]
+    internal static class RefreshOnOnSpawn
+    {
+        private static void Postfix(ImmigrantScreen __instance)
+        {
+            if (ModUtils.HasRefreshMod()) return;
+            Traverse.Create(__instance).Field("rejectButton").GetValue<KButton>()
+                .GetComponentInChildren<LocText>()
+                .text = (Localization.GetLocale() != null &&
+                         Localization.GetLocale().Lang == Localization.Language.Chinese)
+                ? "再来亿次"
+                : (string) UI.IMMIGRANTSCREEN.SHUFFLE;
+        }
+    }
+
     [HarmonyPatch(typeof(ImmigrantScreen), "OnRejectAll")]
     internal static class Refresh
     {
-        private const string RefreshModId = "1724518038";
-
         private static float _lastTime;
-
-
-        private static bool HasRefreshMod()
-        {
-            return ModUtils.HasMod(RefreshModId);
-        }
 
         public static bool Prefix(ImmigrantScreen __instance)
         {
-            if (HasRefreshMod())
+            if (ModUtils.HasRefreshMod())
             {
                 Debug.Log("启用了刷新选人Mod，跳过");
                 return true;
