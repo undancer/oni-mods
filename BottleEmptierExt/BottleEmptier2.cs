b2c1d744167815f43858b1bd7d0f6fc74c4932aa
undancer <undancer@gmail.com>
2019-10-10 12:40:06 +0800

    修改过的空瓶器

---
+++ BottleEmptierExt/BottleEmptier2.cs
@@ -0,0 +1,29 @@
+using KSerialization;
+using UnityEngine;
+
+namespace BottleEmptierExt
+{
+    [SerializationConfig(MemberSerialization.OptIn)]
+    public class BottleEmptier2 : BottleEmptier, IUserControlledCapacity
+    {
+        [Serialize] private float userMaxCapacity = float.PositiveInfinity;
+
+        public virtual float UserMaxCapacity
+        {
+            get => Mathf.Min(userMaxCapacity, GetComponent<Storage>().capacityKg * 1000f);
+            set => userMaxCapacity = value;
+        }
+
+        public float AmountStored => GetComponent<Storage>().MassStored();
+
+
+        public float MinCapacity => 0.0f;
+
+        public float MaxCapacity => GetComponent<Storage>().capacityKg * 1000f;
+
+
+        public bool WholeValues => false;
+
+        public LocString CapacityUnits => GameUtil.GetCurrentMassUnit(true);
+    }
+}
---
+++ BottleEmptierExt/Patch.cs
@@ -0,0 +1,54 @@
+﻿using BottleEmptierExt;
+using Harmony;
+using TUNING;
+using UnityEngine;
+
+namespace BottleEmptierExt
+{
+    [HarmonyPatch(typeof(BottleEmptierConfig), "ConfigureBuildingTemplate")]
+    public static class BottleEmptierConfigConfigureBuildingTemplate
+    {
+        public static bool Prefix(GameObject go, Tag prefab_tag)
+        {
+            Prioritizable.AddRef(go);
+            var storage = go.AddOrGet<Storage>();
+            storage.storageFilters = STORAGEFILTERS.LIQUIDS;
+            storage.showInUI = true;
+            storage.showDescriptor = true;
+            storage.capacityKg = 200f;
+            go.AddOrGet<TreeFilterable>();
+            go.AddOrGet<BottleEmptier2>();
+
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(BottleEmptier.StatesInstance), "CreateChore")]
+    public static class L
+    {
+        public static bool Prefix(BottleEmptier.StatesInstance __instance)
+        {
+            var controller = __instance.GetComponent<KBatchedAnimController>();
+            var tags = __instance.GetComponent<TreeFilterable>().GetTags();
+            if (tags == null || tags.Length == 0)
+            {
+                controller.TintColour = __instance.master.noFilterTint;
+            }
+            else
+            {
+                controller.TintColour = __instance.master.filterTint;
+                var forbiddenTags = !__instance.master.allowManualPumpingStationFetching
+                    ? new[] {GameTags.LiquidSource}
+                    : new Tag[0];
+
+                var amount = ((BottleEmptier2) __instance.master).UserMaxCapacity / 1000f;
+
+                var chore = new FetchChore(Db.Get().ChoreTypes.StorageFetch, __instance.GetComponent<Storage>(), amount,
+                    __instance.GetComponent<TreeFilterable>().GetTags(), (Tag[]) null, forbiddenTags);
+                Traverse.Create(__instance).Field("chore").SetValue(chore);
+            }
+
+            return false;
+        }
+    }
+}
