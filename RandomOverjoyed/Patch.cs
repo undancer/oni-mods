2b004594d0ebac8bbbb5380022891f92e1d7b73f
undancer <undancer@gmail.com>
2019-11-23 01:38:57 +0800

    在官方算法的基础上修改，允许触发概率超过固定的50%，超出条件按官方算法计算

--- RandomOverjoyed/Patch.cs
+++ RandomOverjoyed/Patch.cs
@@ -1,17 +1,34 @@
-﻿using System;
-using Harmony;
+﻿using Harmony;
+using Klei.AI;
+using TUNING;
 
 namespace RandomOverjoyed
 {
-    [HarmonyPatch(typeof(JoyBehaviourMonitor.Instance),"ShouldBeOverjoyed")]
+    [HarmonyPatch(typeof(JoyBehaviourMonitor.Instance), "ShouldBeOverjoyed")]
     public class JoyBehaviourMonitorInstancePatch
     {
-
-        public static bool Prefix(ref bool __result)
+        public static bool Prefix(JoyBehaviourMonitor.Instance __instance, ref bool __result)
         {
-            __result = (double) UnityEngine.Random.Range(0.0f, 100f) <= 50;
+            var instance = Traverse.Create(__instance);
+            var qav = instance.Field("qolAttribute").GetValue<AttributeInstance>().GetTotalValue();
+            var eav = instance.Field("expectationAttribute").GetValue<AttributeInstance>().GetTotalValue();
+
+            var val = MathUtil.ReRange(
+                qav - eav,
+                TRAITS.JOY_REACTIONS.MIN_MORALE_EXCESS,
+                TRAITS.JOY_REACTIONS.MAX_MORALE_EXCESS,
+                TRAITS.JOY_REACTIONS.MIN_REACTION_CHANCE,
+                TRAITS.JOY_REACTIONS.MAX_REACTION_CHANCE
+            );
+            var rdm = UnityEngine.Random.Range(0.0f, 100.0f);
+            Debug.Log("val: " + val + " random:" + rdm);
+            if (val < 50)
+            {
+                val = 50;
+            }
+
+            __result = rdm <= val;
             return false;
         }
-        
     }
 }
