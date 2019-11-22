a01d0b455c03814e78ccbe4b621a03f479054729
undancer <undancer@gmail.com>
2019-11-22 23:33:51 +0800

    修改代码错误

--- RandomOverjoyed/Patch.cs
+++ RandomOverjoyed/Patch.cs
@@ -3,11 +3,11 @@ using Harmony;
 
 namespace RandomOverjoyed
 {
-    [HarmonyPatch(typeof(JoyBehaviourMonitor),"ShouldBeOverjoyed")]
-    public class JoyBehaviourMonitorPatch
+    [HarmonyPatch(typeof(JoyBehaviourMonitor.Instance),"ShouldBeOverjoyed")]
+    public class JoyBehaviourMonitorInstancePatch
     {
 
-        public bool Prefix(ref bool __result)
+        public static bool Prefix(ref bool __result)
         {
             __result = (double) UnityEngine.Random.Range(0.0f, 100f) <= 50;
             return false;
