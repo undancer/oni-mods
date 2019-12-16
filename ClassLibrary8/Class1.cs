043d59793d77900663b457421efd3e6fbe151014
undancer <undancer@gmail.com>
2019-12-16 16:24:42 +0800

    添加国际化方案

---
+++ ClassLibrary8/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using ClassLibrary8.lang;
+using Harmony;
+
+namespace ClassLibrary8
+{
+    public class ModLoader
+    {
+        public static void OnLoad()
+        {
+            Debug.Log("OnLoad !!");
+            Localization.RegisterForTranslation(typeof(Class1));
+            
+            
+        }
+    }
+
+    [HarmonyPatch(typeof(Strings),"Add")]
+    public class Class2
+    {
+        public static void Postfix(IEnumerable<string> value)
+        {
+            Debug.Log("----------");
+            foreach (var str in value)
+            {
+                Debug.Log("LANG -> " +  str);
+            }
+            Debug.Log("----------");
+        }
+    }
+}
---
+++ ClassLibrary8/lang/Class1.cs
@@ -0,0 +1,18 @@
+namespace ClassLibrary8.lang
+{
+    // xgettext Class1.cs  -a -o ./templates.pot
+    public class Class1
+    {
+        public static LocString TEST = (LocString) "Test";
+    }
+
+    public class Class2
+    {
+        public void foo()
+        {
+//            GetString, GetPluralString:1,2, GetParticularString:1c,2, GetParticularPluralString:1c,2,3.
+
+
+        }
+    }
+}
