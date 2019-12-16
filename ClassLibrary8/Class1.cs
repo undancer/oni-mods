a1c44e48f56badf1c5638957532dc0c2f7b53a53
undancer <undancer@gmail.com>
2019-12-16 17:20:39 +0800

    加载语言包的方式

--- ClassLibrary8/Class1.cs
+++ ClassLibrary8/Class1.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using ClassLibrary8.lang;
 using Harmony;
+using KMod;
 
 namespace ClassLibrary8
 {
@@ -9,13 +10,25 @@ namespace ClassLibrary8
         public static void OnLoad()
         {
             Debug.Log("OnLoad !!");
+#if DEBUG
+            ModUtil.RegisterForTranslation(typeof(Class1));
+#else
             Localization.RegisterForTranslation(typeof(Class1));
-            
-            
+#endif
         }
     }
 
-    [HarmonyPatch(typeof(Strings),"Add")]
+    [HarmonyPatch(typeof(Localization), nameof(Localization.Initialize))]
+    public class Class3
+    {
+        public static void Postfix()
+        {
+            Debug.Log(lang.Class1.TEST);
+            Debug.Log(STRINGS.UI.ALERTS);
+        }
+    }
+
+//    [HarmonyPatch(typeof(Strings),"Add")]
     public class Class2
     {
         public static void Postfix(IEnumerable<string> value)
@@ -23,8 +36,9 @@ namespace ClassLibrary8
             Debug.Log("----------");
             foreach (var str in value)
             {
-                Debug.Log("LANG -> " +  str);
+                Debug.Log("LANG -> " + str);
             }
+
             Debug.Log("----------");
         }
     }
