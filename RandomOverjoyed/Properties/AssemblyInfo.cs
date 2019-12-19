8aa6d59d68c1afdf0abf1344d6890d50251dbac2
undancer <undancer@gmail.com>
2019-12-19 15:48:59 +0800

    删除版本号定义，用插件来产生

--- RandomOverjoyed/Properties/AssemblyInfo.cs
+++ RandomOverjoyed/Properties/AssemblyInfo.cs
@@ -20,16 +20,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("F9095A87-1899-4AF0-9FFE-754E0A6C0385")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
