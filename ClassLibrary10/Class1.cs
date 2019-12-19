44bc8b5c45852ce2d53b8217c374688466c4995d
undancer <undancer@gmail.com>
2019-12-20 00:00:55 +0800

    尝试抓取资源图片

---
+++ ClassLibrary10/Class1.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Harmony;
+using STRINGS;
+using UnityEngine;
+
+namespace ClassLibrary10
+{
+    public static class C
+    {
+        
+        public static Texture2D DeCompress(this Texture2D source)
+        {
+            RenderTexture renderTex = RenderTexture.GetTemporary(
+                source.width,
+                source.height,
+                0,
+                RenderTextureFormat.Default,
+                RenderTextureReadWrite.Linear);
+
+            Graphics.Blit(source, renderTex);
+            RenderTexture previous = RenderTexture.active;
+            RenderTexture.active = renderTex;
+            Texture2D readableText = new Texture2D(source.width, source.height);
+            readableText.ReadPixels(new Rect(0, 0, renderTex.width, renderTex.height), 0, 0);
+            readableText.Apply();
+            RenderTexture.active = previous;
+            RenderTexture.ReleaseTemporary(renderTex);
+            return readableText;
+        }
+    }
+    
+    [HarmonyPatch(typeof(CodexScreen),"OnActivate")]
+    public class Class1
+    {
+        
+
+        public static void Postfix()
+        {
+            Debug.Log(" !! Postfix !! ");
+            
+            foreach (var element in ElementLoader.elements)
+            {
+                Tuple<Sprite, Color> sprite =                 Def.GetUISprite(element);
+                var image =                 new CodexImage(32,32,sprite);
+                var path = Path.Combine("/Users/undancer/oni",                 element.tag.Name + ".png");
+                Debug.Log(path);
+                Debug.Log(image.sprite);
+                Debug.Log(image.sprite.texture);
+                var data = image.sprite.texture.DeCompress().EncodeToJPG();
+                File.WriteAllBytes(path,data);
+            }
+
+        }
+        
+    }
+}
---
+++ ClassLibrary10/Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ClassLibrary10")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ClassLibrary10")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("854D419E-7172-41B7-A4D7-ED8C50106C9C")]
