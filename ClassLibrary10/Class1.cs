3a6942791bd85b03f6d72a727a93cec69544742f
undancer <undancer@gmail.com>
2019-12-22 00:06:16 +0800

    删除多余代码

--- ClassLibrary10/Class1.cs
+++ ClassLibrary10/Class1.cs
@@ -4,11 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
 using Harmony;
-using Klei;
-using STRINGS;
 using UnityEngine;
-using UnityEngine.UI;
-using Object = UnityEngine.Object;
 
 namespace ClassLibrary10
 {
@@ -258,89 +254,8 @@ namespace ClassLibrary10
 
                 SaveImage(image);
             }
-            
+
             Debug.Log("文件生成完成 !!");
-
-
-            return;
-
-            var sprites =
-                (Dictionary<Tuple<KAnimFile, string, bool>, Sprite>) AccessTools.Field(typeof(Def), "knownUISprites")
-                    .GetValue(null);
-            foreach (var keyValuePair in sprites)
-            {
-                var tuple = keyValuePair.Key;
-                var animFile = tuple.first;
-                var animName = tuple.second;
-                var centered = tuple.third;
-                var sprite = keyValuePair.Value;
-
-                Debug.Log(animFile);
-                Debug.Log(animName);
-                Debug.Log(centered);
-                Debug.Log(sprite);
-                Debug.Log("");
-            }
-
-            // 物质
-            // 元素
-            // CreatureBrain
-            // SpaceArtifact
-
-            return;
-
-            foreach (var element in ElementLoader.elements.Where(element => !element.IsVacuum))
-            {
-                try
-                {
-                    var image = new CodexImage(32, 32, Def.GetUISprite(element));
-                    var sprite = image.sprite;
-                    var color = image.color;
-
-                    var r = sprite.textureRect;
-                    var texture = sprite.texture.DeCompress();
-                    Texture2D texture2D = null;
-                    try
-                    {
-                        texture2D = texture.CropTexture((int) r.xMin, (int) r.yMin, (int) r.width, (int) r.height);
-                    }
-                    catch (IndexOutOfRangeException e)
-                    {
-                        Debug.Log(element.tag.Name + " 导出失败");
-                        Debug.Log(r);
-                    }
-                    catch (NullReferenceException e)
-                    {
-                        Debug.Log(element.tag.Name + " 导出失败");
-                        Debug.Log(r);
-                    }
-
-                    if (texture2D != null)
-                    {
-                        texture = texture2D;
-                    }
-
-                    if (!color.Equals(Color.white))
-                    {
-                        texture = texture.FillTexture(color);
-                    }
-
-
-                    var data = texture.EncodeToPNG();
-                    var path = Path.Combine("/Users/undancer/oni", element.tag.Name.ToUnderscore() + ".png");
-                    File.WriteAllBytes(path, data);
-
-                    //    var path = Path.Combine("/Users/undancer/oni",                 texture.name + ".png");
-                    //    var data = texture.DeCompress().EncodeToPNG();
-                    //    File.WriteAllBytes(path,data);
-                }
-                catch (Exception e)
-                {
-                    Debug.Log(element.tag.Name);
-                    Debug.LogException(e);
-                    Debug.Log("");
-                }
-            }
         }
     }
 }
