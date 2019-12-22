5a89478151632fa1d1f3249000fbb4f8fb2b2f52
undancer <undancer@gmail.com>
2019-12-23 02:48:49 +0800

    项目改名

--- CodexProbe/Properties/AssemblyInfo.cs
+++ CodexProbe/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ClassLibrary10")]
+[assembly: AssemblyTitle("CodexProbe")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ClassLibrary10")]
+[assembly: AssemblyProduct("CodexProbe")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
