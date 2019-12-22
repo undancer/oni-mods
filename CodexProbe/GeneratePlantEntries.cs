e1ad25c123d13f833e27bda916a0c69340aa099f
undancer <undancer@gmail.com>
2019-12-23 04:53:37 +0800

    植物

--- CodexProbe/GeneratePlantEntries.cs
+++ CodexProbe/GeneratePlantEntries.cs
@@ -1,4 +1,6 @@
+using System.Collections.Generic;
 using Harmony;
+using UnityEngine;
 
 namespace CodexProbe
 {
@@ -8,6 +10,31 @@ namespace CodexProbe
         public static void Postfix()
         {
             Debug.Log("GeneratePlantEntries");
+            var cache = new List<string>();
+            
+            var prefabsWithComponent = Assets.GetPrefabsWithComponent<Harvestable>();
+            prefabsWithComponent.AddRange(Assets.GetPrefabsWithComponent<WiltCondition>());
+          
+            foreach (var go in prefabsWithComponent)
+            {
+                var name = go.PrefabID().ToString();
+
+                if (cache.Contains(name))
+                {
+                    continue;
+                }
+
+                cache.Add(name);
+
+                var sprite = Def.GetUISprite(go).first;
+
+                ImageUtils.SaveImage(new Image
+                {
+                    prefixes = new[] {"ASSETS/PLANTS", name.ToUpper()},
+                    sprite = sprite,
+                    color = Color.white
+                });
+            }
         }
     }
 }
