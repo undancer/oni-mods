ee93b0e61f643b39014b36db2de137b0769fdbb3
undancer <undancer@gmail.com>
2019-12-23 05:10:21 +0800

    疾病

--- CodexProbe/GenerateDiseaseEntries.cs
+++ CodexProbe/GenerateDiseaseEntries.cs
@@ -1,4 +1,5 @@
 using Harmony;
+using UnityEngine;
 
 namespace CodexProbe
 {
@@ -8,6 +9,18 @@ namespace CodexProbe
         public static void Postfix()
         {
             Debug.Log("GenerateDiseaseEntries");
+            foreach (var resource in Db.Get().Diseases.resources)
+            {
+                if (resource.Disabled) continue;
+                var name = resource.Id;
+                var sprite = Assets.GetSprite( "overlay_disease");
+                ImageUtils.SaveImage(new Image
+                {
+                    prefixes = new[] {"ASSETS/DISEASE", name.ToUpper()},
+                    sprite = sprite,
+                    color = Color.white,
+                });
+            }
         }
     }
 }
