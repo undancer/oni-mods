b157b1ef042d848237c30d34ee8f1b873611af30
undancer <undancer@gmail.com>
2019-12-23 05:53:58 +0800

    生物

--- CodexProbe/GenerateCreatureEntries.cs
+++ CodexProbe/GenerateCreatureEntries.cs
@@ -1,4 +1,6 @@
+using System.Collections.Generic;
 using Harmony;
+using UnityEngine;
 
 namespace CodexProbe
 {
@@ -8,6 +10,41 @@ namespace CodexProbe
         public static void Postfix()
         {
             Debug.Log("GenerateCreatureEntries");
+            var brains = Assets.GetPrefabsWithComponent<CreatureBrain>();
+            if (brains != null)
+            {
+                foreach (var go in brains)
+                {
+                    if (go.GetDef<BabyMonitor.Def>() == null)
+                    {
+                        var name = go.PrefabID().ToString();
+                        var prefab = Assets.TryGetPrefab((Tag) (go.PrefabID() + "Baby"));
+                        var brain = go.GetComponent<CreatureBrain>();
+                        var species = brain.species.ToString();
+                        if (prefab != null)
+                        {
+                            var prefabName = prefab.PrefabID().ToString();
+                            var prefabSprite = Def.GetUISprite(prefab).first;
+
+                            ImageUtils.SaveImage(new Image
+                            {
+                                prefixes = new[] {"ASSETS/CREATURES/SPECIES", species.ToUpper(), prefabName.ToUpper()},
+                                sprite = prefabSprite,
+                                color = Color.white,
+                            });
+                        }
+                        
+                        var sprite = Def.GetUISprite(go).first;
+
+                        ImageUtils.SaveImage(new Image
+                        {
+                            prefixes = new[] {"ASSETS/CREATURES/SPECIES", species.ToUpper(), name.ToUpper() },
+                            sprite = sprite,
+                            color = Color.white,
+                        });
+                    }
+                }
+            }
         }
     }
 }
