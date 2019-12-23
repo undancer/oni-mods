1119443785d9a15605aa4d2e625d3e5cfc7cf434
undancer <undancer@gmail.com>
2019-12-23 09:58:31 +0800

    重新产生配方和摆件

--- CodexProbe/GenerateBuildingEntries.cs
+++ CodexProbe/GenerateBuildingEntries.cs
@@ -13,7 +13,6 @@ namespace CodexProbe
         {
             Debug.Log("GenerateBuildingEntries");
             const string prefix = "BUILD_CATEGORY_";
-            var fabricatorCache = new List<string>();
             foreach (var info in BUILDINGS.PLANORDER)
             {
                 var category = HashCache.Get().Get(info.category);
@@ -31,74 +30,67 @@ namespace CodexProbe
                         sprite = buildingDef.GetUISprite(),
                         color = Color.white,
                     });
-
-                    var buildingGo = buildingDef.BuildingComplete;
-                    // Fabricator
-
-                    var fabricator = buildingGo.GetComponent<ComplexFabricator>();
-                    if (fabricator != null)
-                    {
-                        foreach (var recipe in fabricator.GetRecipes())
-                        {
-                            Debug.Log("RECIPE : " + recipe.id);
-                            
-                            var elements = new TagSet();
-                            //原料
-                            foreach (var element in recipe.ingredients)
-                            {
-                                elements.Add(element.material);
-                            }
-                            //制品
-                            foreach (var element in recipe.results)
-                            {
-                                elements.Add(element.material);
-                            }
-                            
-                            foreach (var element in elements)
-                            {
-                                var elementGo = Assets.GetPrefab(element);
-                                if (elementGo == null) continue;
-                                var elementName = elementGo.PrefabID().ToString();
-                                var elementTuple = Def.GetUISprite(element);
-                                var elementSprite = elementTuple.first;
-                                var elementColor = elementTuple.second;
-
-                                ImageUtils.SaveImage(new Image
-                                {
-                                    prefixes = new[] {"ASSETS/EL", name.ToUpper(), elementName.ToUpper()},
-                                    sprite = elementSprite,
-                                    color = elementColor,
-                                });
-                            }
-                        }
-                    }
-
-
-                    // Receptacle
-                    var plot = buildingGo.GetComponent<SingleEntityReceptacle>();
-                    if (plot == null) continue;
-                    foreach (var depositObjectTag in plot.possibleDepositObjectTags)
-                    {
-                        var prefabsWithTag = Assets.GetPrefabsWithTag(depositObjectTag);
-                        foreach (var receptacleGo in prefabsWithTag)
-                        {
-                            if (receptacleGo == null) continue;
-                            var receptacleId = receptacleGo.PrefabID().ToString();
-                            if (!receptacleId.StartsWith("artifact_")) continue;
-                            var receptacleName = receptacleId;
-                            var receptacleTuple = Def.GetUISprite(receptacleGo);
-                            var receptacleSprite = receptacleTuple.first;
-                            var receptacleColor = receptacleTuple.second;
-
-                            ImageUtils.SaveImage(new Image
-                            {
-                                prefixes = new[] {"ASSETS/ARTIFACT", receptacleName.ToUpper()},
-                                sprite = receptacleSprite,
-                                color = receptacleColor,
-                            });
-                        }
-                    }
                 }
+            }
+
+            GenerateRecipeEntries();
+            GenerateArtifactEntries();
+        }
+
+        public static void GenerateRecipeEntries()
+        {
+            var recipes = ComplexRecipeManager.Get().recipes;
+            var elementSet = new TagSet();
+            foreach (var recipe in recipes)
+            {
+                // 配方
+                Debug.Log("RECIPE : " + recipe.id);
+                // 原料
+                foreach (var recipeElement in recipe.ingredients)
+                {
+                    elementSet.Add(recipeElement.material);
+                }
+                // 制品
+                foreach (var recipeElement in recipe.results)
+                {
+                    elementSet.Add(recipeElement.material);
+                }
+            }
+
+            foreach (var element in elementSet)
+            {
+                var elementGo = Assets.GetPrefab(element);
+                if (elementGo == null) continue;
+                var elementName = elementGo.PrefabID().ToString();
+                var elementTuple = Def.GetUISprite(element);
+                var elementSprite = elementTuple.first;
+                var elementColor = elementTuple.second;
+
+                ImageUtils.SaveImage(new Image
+                {
+                    prefixes = new[] {"ASSETS/RECIPES", elementName.ToUpper()},
+                    sprite = elementSprite,
+                    color = elementColor,
+                });
+            }
+        }
+
+        public static void GenerateArtifactEntries()
+        {
+            var config = new ArtifactConfig();
+            foreach (var go in config.CreatePrefabs())
+            {
+                var name = go.PrefabID().ToString();
+                var tuple = Def.GetUISprite(go);
+                var sprite = tuple.first;
+                var color = tuple.second;
+                
+                ImageUtils.SaveImage(new Image
+                {
+                    prefixes = new[] {"ASSETS/ARTIFACTS", name.ToUpper()},
+                    sprite = sprite,
+                    color = color,
+                });
             }
         }
     }
