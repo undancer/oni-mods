e4ce4ed38f7fa0b54bae9b5151fad8e7d65f55b9
undancer <undancer@gmail.com>
2020-01-05 17:53:10 +0800

    增加打开存档目录的按钮

---
+++ ClassLibrary5/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text.RegularExpressions;
+using Harmony;
+using undancer.Commons;
+using UnityEngine;
+
+namespace ClassLibrary5
+{
+    [HarmonyPatch(typeof(LoadScreen), "OnPrefabInit")]
+    public static class Class1
+    {
+        public static void Postfix(
+            KButton ___loadButton
+        )
+        {
+            var openButton = Util.KInstantiateUI<KButton>(
+                ___loadButton.gameObject,
+                ___loadButton.transform.parent.gameObject,
+                true
+            );
+            var p = openButton.transform.position;
+            openButton.transform.position = new Vector3(p.x - 130, p.y);
+            openButton.name = "OpenButton";
+            openButton.GetComponentInChildren<LocText>().text = "Open...";
+            openButton.onClick += delegate
+            {
+                var url = Util.RootFolder();
+                FinderUtils.OpenInFileBrowser(url);
+            };
+        }
+    }
+}
