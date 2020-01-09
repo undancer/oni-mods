593eebde7553a68106e72ac81701c602b0bb97d4
undancer <undancer@gmail.com>
2020-01-09 17:31:12 +0800

    添加新的mod

---
+++ ClassLibrary10/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using Harmony;
+
+namespace ClassLibrary10
+{
+
+    public class Hook
+    {
+        public static bool AlwaysTrue()
+        {
+            Debug.Log("DistributionPlatform.Initialized");
+            Debug.Log("================================");
+            Debug.Log(DistributionPlatform.Initialized);
+            Debug.Log("================================");
+            return true;
+        }
+    }
+    
+    [HarmonyPatch(typeof(MainMenu),"OnPrefabInit")]
+    public static class MainMenuOnPrefabInitPatch
+    {
+        
+        public static IEnumerable<CodeInstruction> Transpiler(MethodBase original,
+            IEnumerable<CodeInstruction> instructions)
+        {
+            var method = AccessTools.Method(typeof(Hook), nameof(Hook.AlwaysTrue));
+            foreach (var instruction in instructions)
+            {
+                if (instruction.ToString() == "call Boolean get_Initialized()")
+                {
+                    instruction.operand = method;
+                }
+                yield return instruction;
+            }
+        }
+    }
+}
