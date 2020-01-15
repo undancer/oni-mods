3e3e3a62718df64486594aabd3519f64ef42d123
undancer <undancer@gmail.com>
2020-01-15 23:05:16 +0800

    添加mod

---
+++ GenericFabricator/GeneratedBuildingsLoadGeneratedBuildingsPatch.cs
@@ -0,0 +1,13 @@
+using Harmony;
+
+namespace GenericFabricator
+{
+    [HarmonyPatch(typeof(GeneratedBuildings), nameof(GeneratedBuildings.LoadGeneratedBuildings))]
+    public static class GeneratedBuildingsLoadGeneratedBuildingsPatch
+    {
+        public static void Prefix()
+        {
+            ModUtil.AddBuildingToPlanScreen(new HashedString("Refining"), GenericFabricatorConfig.ID);
+        }
+    }
+}
---
+++ GenericFabricator/GenericFabricatorConfigCreateBuildingDefPatch.cs
@@ -0,0 +1,13 @@
+﻿using Harmony;
+
+namespace GenericFabricator
+{
+    [HarmonyPatch(typeof(GenericFabricatorConfig), nameof(GenericFabricatorConfig.CreateBuildingDef))]
+    public static class GenericFabricatorConfigCreateBuildingDefPatch
+    {
+        public static void Postfix(ref BuildingDef __result)
+        {
+            __result.Deprecated = false;
+        }
+    }
+}
