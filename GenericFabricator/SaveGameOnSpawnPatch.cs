0ec1f27fedc0cc281a27fef332a411dc96859e81
undancer <undancer@gmail.com>
2020-01-16 10:24:49 +0800

    调整配方的顺序

--- GenericFabricator/SaveGameOnSpawnPatch.cs
+++ GenericFabricator/SaveGameOnSpawnPatch.cs
@@ -7,40 +7,64 @@ namespace GenericFabricator
     [HarmonyPatch(typeof(SaveGame), "OnPrefabInit")]
     public static class SaveGameOnSpawnPatch
     {
+        private static bool _inited;
+
+        private static List<string> fabricatorList = new List<string>
+        {
+            MicrobeMusherConfig.ID, //食物压制器
+            CookingStationConfig.ID, //电动烤炉
+            GourmetCookingStationConfig.ID, //燃气灶
+            KilnConfig.ID, //窑炉
+            RockCrusherConfig.ID, //碎石机
+            MetalRefineryConfig.ID, //金属精炼器
+            GlassForgeConfig.ID, //玻璃熔炉
+            SupermaterialRefineryConfig.ID, //分子熔炉
+            ClothingFabricatorConfig.ID, //纺织机
+            SuitFabricatorConfig.ID, //太空服锻造
+            EggCrackerConfig.ID, //打蛋器
+            ApothecaryConfig.ID, //制药桌
+        };
+
         public static void Postfix()
         {
+            if (_inited) return;
             var recipeIdList = new List<string>();
             foreach (var recipe in ComplexRecipeManager.Get().recipes)
             {
                 if (!recipe.fabricators.First().Equals(new Tag(GenericFabricatorConfig.ID)))
                 {
-                    {
-                        recipeIdList.Add(recipe.id);
-                    }
+                    recipeIdList.Add(recipe.id);
                 }
             }
 
             foreach (var recipeId in recipeIdList)
             {
                 var recipe = ComplexRecipeManager.Get().GetRecipe(recipeId);
-                
-                var recipeObj = new ComplexRecipe(
-                    ComplexRecipeManager.MakeRecipeID(GenericFabricatorConfig.ID,
-                        recipe.ingredients,
-                        recipe.results)
-                    , recipe.ingredients,
-                    recipe.results)
+                var fabricator = recipe.id.Substring(0, recipe.id.IndexOf('_'));
+                var index = fabricatorList.IndexOf(fabricator);
+
+                if (index >= 0)
                 {
-                    time = recipe.time,
-                    description = recipe.description,
-                    nameDisplay = recipe.nameDisplay,
-                    fabricators = new List<Tag>
+                    var recipeObj = new ComplexRecipe(
+                        ComplexRecipeManager.MakeRecipeID(GenericFabricatorConfig.ID,
+                            recipe.ingredients,
+                            recipe.results)
+                        , recipe.ingredients,
+                        recipe.results)
                     {
-                        new Tag(GenericFabricatorConfig.ID)
-                    },
-                    sortOrder = recipe.sortOrder,
-                };
+                        time = recipe.time,
+                        FabricationVisualizer = recipe.FabricationVisualizer,
+                        nameDisplay = recipe.nameDisplay,
+                        description = recipe.description,
+                        fabricators = new List<Tag>
+                        {
+                            new Tag(GenericFabricatorConfig.ID)
+                        },
+                        sortOrder = (index + 1) * 1000 + recipe.sortOrder,
+                    };
+                }
             }
+            _inited = true;
         }
     }
 }
