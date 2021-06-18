bf2b59e3ceb8baa587c8baa5982df2b7005cc61b
undancer <>
2021-06-18 21:46:17 +0800

    更新代码并使用task来生成结构

--- BottleEmptierExt/Patch.cs
+++ BottleEmptierExt/Patch.cs
@@ -40,13 +40,16 @@ namespace undancer.BottleEmptierExt
     {
         public static bool Prefix(BottleEmptier.StatesInstance __instance)
         {
-            if (!(__instance.master is IUserControlledCapacity)) return true; 
+            if (!(__instance.master is IUserControlledCapacity)) return true;
             __instance.GetComponent<KBatchedAnimController>();
             var tags = __instance.GetComponent<TreeFilterable>().GetTags();
-            var forbidden_tags = !__instance.master.allowManualPumpingStationFetching ? new Tag[1]{ GameTags.LiquidSource } : new Tag[0];
+            var forbidden_tags = !__instance.master.allowManualPumpingStationFetching
+                ? new Tag[1] {GameTags.LiquidSource}
+                : new Tag[0];
             var amount = ((BottleEmptier2) __instance.master).UserMaxCapacity / 1000f;
             var component = __instance.GetComponent<Storage>();
-            var chore = new FetchChore(Db.Get().ChoreTypes.StorageFetch, component, amount, tags, forbidden_tags: forbidden_tags);
+            var chore = new FetchChore(Db.Get().ChoreTypes.StorageFetch, component, amount, tags,
+                forbidden_tags: forbidden_tags);
             Traverse.Create(__instance).Field("chore").SetValue(chore);
             return false;
         }
