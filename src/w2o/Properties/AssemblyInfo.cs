d0fa41d3a8e9c0933d9ea46334a6d3088564dad4
undancer <>
2021-09-22 17:33:59 +0800

    修改最后的版本号

--- src/w2o/Properties/AssemblyInfo.cs
+++ src/w2o/Properties/AssemblyInfo.cs
@@ -20,16 +20,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("A3143F86-6268-4026-B38F-3FFD2CBDFED2")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
